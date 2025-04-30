4d4ed5454137aeb81441c1784ba13bc8fc69b49b
DuzzDev <kaykyalmeida1598@gmail.com>
2025-04-29 21:10:08 -0300

    create_systemsearch_customerID

--- src/App.js
+++ src/App.js
@@ -1,22 +1,36 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { ethers } from 'ethers';
+import ABI from './abi.json';
 
 function App() {
+
+  const [customerId, setCustomerId] = useState("");
+  const [message, setMessage] = useState("");
+
+  
+  }
+
+  
+
+  function onSearchClick(){
+    setMessage("");
+    console.log("Customer ID: ", customerId);
+  }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          <label>
+            Customer ID:
+            <input type="number" value={customerId}  onChange={(evt) => setCustomerId(evt.target.value)}/>
+          </label>
+          <input type="button" value="Search" onClick={onSearchClick}/>
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p>
+          {message}
+        </p>
       </header>
     </div>
   );
